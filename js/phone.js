b58a7584aefe949ea8a2b5ff4136479a7f5d2fdb
Mohotazim-Bosunia <zimhossen0@gmail.com>
2025-04-18 23:41:14 +0600

    2nd commit

---
+++ js/phone.js
@@ -0,0 +1,57 @@
+/**
+ * function loadPhone(){
+    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    .then(res=>res.json())
+    .then(data=>console.log(data.data))
+}
+loadPhone()
+ */
+
+const loadPhone= async (searchText)=> {
+  const res=await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+  const data=await res.json()
+  const phones=data.data;
+  //console.log(phones)
+  displauPhones(phones)
+}
+
+const displauPhones=phones=>{
+        //1.get id
+    const phoneContainer=document.getElementById("phone-container")
+    //clear phone container cards before adding new cards
+    phoneContainer.textContent=" "
+    phones.forEach(phone=>{
+        console.log(phone)
+        //2.Create a div
+        const phoneCard=document.createElement('div')
+        phoneCard.classList="card bg-lime-100 p-4 shadow-sm"
+        //3.set inner html
+        phoneCard.innerHTML=`
+        <figure>
+                  <img
+                    src="${phone.image}"
+                    alt="Shoes" />
+                </figure>
+                <div class="card-body">
+                  <h2 class="card-title">${phone.phone_name
+                  }</h2>
+                  <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                  <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+        `
+        //appoand child
+        phoneContainer.appendChild(phoneCard)
+    })
+    
+}
+//handle Search button
+const handleSearch=()=>{
+  const searchField=document.getElementById("search-field");
+  const searchText=searchField.value;
+  console.log(searchText)
+  
+  loadPhone(searchText)
+}
+//loadPhone()
